7595b6d146a7b4624b1a5a77bac6b9dec9fa8349
Ross Tungol <ross@proudcloud.io>
2023-11-22 22:34:42 +0800

    Updates HeroBanner component

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,5 +1,25 @@
+import Link from "next/link"
+
 const HeroBanner = () => {
-  return <div>HeroBanner</div>
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>Small text</p>
+        <h3>Mid text</h3>
+        <img src='' alt='headphones' className='hero-banner-image' />
+
+        <div>
+          <Link href='/product/ID'>
+            <button type='button'>Button text</button>
+          </Link>
+          <div className='desc'>
+            <h5>desciption</h5>
+            <p>description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HeroBanner
