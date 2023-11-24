ee6330685d8dcefaad0eacc85965566d3e1eeb2f
Ross Tungol <ross@proudcloud.io>
2023-11-24 23:47:25 +0800

    Updates Product component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,5 +1,22 @@
-const Product = () => {
-  return <div>Product</div>
+import Link from "next/link"
+import { urlFor } from "../lib/client"
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <Link href={`/product/${slug.current}`}>
+      <div className='product-card'>
+        <img
+          src={urlFor(image && image[0])}
+          width={250}
+          height={250}
+          alt={name}
+          className='product-image'
+        />
+        <p className='product-name'>{name}</p>
+        <p className='product-price'>${price}</p>
+      </div>
+    </Link>
+  )
 }
 
 export default Product
