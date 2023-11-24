0ed91f32bd979768e028e1ff3a646b4777e33352
Ross Tungol <ross@proudcloud.io>
2023-11-25 02:50:13 +0800

    Updates Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,15 @@
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai"
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className='footer-container'>
+      <p>2023 store All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
 }
 
 export default Footer
