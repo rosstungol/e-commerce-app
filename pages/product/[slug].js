70d85ed337e011f701e973fd315b64320691a662
Ross Tungol <ross@proudcloud.io>
2023-11-28 19:57:24 +0800

    Adds similar product section

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,6 +5,7 @@ import {
   AiOutlineStar
 } from "react-icons/ai"
 import { client, urlFor } from "../../lib/client"
+import { Product } from "../../components"
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product
@@ -58,6 +59,17 @@ const ProductDetails = ({ product, products }) => {
             <button type='button' className='buy-now' onClick=''>
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+
+      <div className='maylike-products-wrapper'>
+        <h2>You may also like</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container track'>
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
