9a6be27e6ae57e21b45a6803b9ce05fdfe6cfab3
Ross Tungol <ross@proudcloud.io>
2023-11-28 19:07:36 +0800

    Adds a static ProductDetails component

---
+++ pages/product/[slug].js
@@ -0,0 +1,15 @@
+const ProductDetails = () => {
+  return (
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            <img src='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetails
