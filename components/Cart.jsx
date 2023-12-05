1a44d54d5be62f1225cd45db37395cadf473c577
Ross Tungol <ross@proudcloud.io>
2023-12-05 19:04:17 +0800

    Adds function for managing item quantities in Cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -13,8 +13,13 @@ import { urlFor } from "../lib/client"
 
 const Cart = () => {
   const cartRef = useRef()
-  const { totalPrice, totalQuantities, cartItems, setShowCart } =
-    useStateContext()
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuantity
+  } = useStateContext()
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -59,11 +64,21 @@ const Cart = () => {
                   <div className=' flex bottom'>
                     <div>
                       <p className='quantity-desc'>
-                        <span className='minus' onClick=''>
+                        <span
+                          className='minus'
+                          onClick={() =>
+                            toggleCartItemQuantity(item._id, "dec")
+                          }
+                        >
                           <AiOutlineMinus />
                         </span>
-                        <span className='num'>0</span>
-                        <span className='plus' onClick=''>
+                        <span className='num'>{item.quantity}</span>
+                        <span
+                          className='plus'
+                          onClick={() =>
+                            toggleCartItemQuantity(item._id, "inc")
+                          }
+                        >
                           <AiOutlinePlus />
                         </span>
                       </p>
